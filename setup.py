52662d9433e372c3ecb5672b1c330a3e6bf62304
Dilawar Singh <dilawars@ncbs.res.in>
2014-12-12 16:11:35 +0530

    Starting a manager.

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md") as f:
+    readme = f.read()
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    ]
+
+setup(
+    name = "ghonchu",
+    version = "0.0.1",
+    description = "A stupid task manager.",
+    long_description = readme,
+    packages = ["package" ],
+    package_data = {},
+    install_requires = [ ],
+    author = "Dilawar Singh",
+    author_email = "dilawars@ncbs.res.in",
+    url = "http://github.com/dilawar/",
+    license='GPL',
+    classifiers=classifiers,
+    entry_points={
+        'console_scripts': 'ghonchu:main'
+        }
+)
